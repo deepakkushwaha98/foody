744874a313f939a7d3d8881bbf7727a77dd09e41
Deepak Kushwaha <deepakkushwaha98@gmail.com>
2026-08-03 19:35:27 +0530

    adminMiddleware file push

---
+++ backend/middlewares/adminMiddleware.js
@@ -0,0 +1,16 @@
+export const isAdmin = (req, res, next) => {
+
+   if (!req.user) {
+      return res.status(401).json({
+         message: "Not authenticated"
+      });
+   }
+
+   if (req.user.role !== "admin") {
+      return res.status(403).json({
+         message: "Access denied. Admin only."
+      });
+   }
+
+   next();
+};
